a69f5dc9f2daa03a3e5fcb91f1675e2480624790
Reece <rmctiernan1@gmail.com>
2018-05-16 22:44:32 +0100

    Add abstract UI

--- src/UI.java
+++ src/UI.java
@@ -1,8 +1,21 @@
+import java.io.EOFException;
+
+import java.io.IOException;
+
 public abstract class UI {
     protected Tasks tasks;
 
     public UI() {
-        tasks = new Tasks();
+        try {
+            tasks = new Tasks();
+            tasks.loadTasks();
+        } catch (EOFException e) {
+            //Do nothing
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public Tasks getTasks() {
